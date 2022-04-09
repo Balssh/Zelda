b2a9d1f43069e8081392724991d97b194d43790b
Balssh <82440615+Balssh@users.noreply.github.com>
2022-04-09 17:31:30 +0300

    Fixed the camera, added a readme

---
+++ README.txt
@@ -0,0 +1,2 @@
+My attempt at creating a zelda-like game, following this tutorial:
+https://youtu.be/QU1pPzEGrqw
--- level.py
+++ level.py
@@ -26,7 +26,7 @@ class Level:
     def run(self):
         # update and draw the game
         #self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         debug((self.player.rect.centerx, self.player.rect.centery))
 
@@ -35,15 +35,15 @@ class YSortCameraGroup(pygame.sprite.Group):
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0]//2
-        self.half_height = self.display_surface.get_size()[1]//2
-        self.offset = pygame.math.Vector2()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(0, 0)
 
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_width
-
+        self.offset.y = player.rect.centery - self.half_height
+        #for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
