b0d3deb3b273c15ad5f22ab90b50ab593b5e2aa4
Balssh <george.bals25@gmail.com>
2022-05-05 22:18:12 +0300

    Added particle effects

--- level.py
+++ level.py
@@ -1,14 +1,15 @@
 import pygame
+from particles import AnimationPlayer
 from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
-
+from particles import AnimationPlayer
 class Level:
     def __init__(self):
         # sprite group setup
@@ -25,6 +26,9 @@ class Level:
 
         # user interface
         self.ui = UI()
+
+        # particles
+        self.animation_player = AnimationPlayer() 
 
     def create_map(self):
         layouts = {
@@ -68,7 +72,7 @@ class Level:
                                     monster_name = 'raccoon'
                                 elif col == '393':
                                     monster_name = 'squid'
-                                Enemy(monster_name, (x, y), [self.visible_sprites, self.attackable_sprites], self.obstacle_sprites, self.damage_player)
+                                Enemy(monster_name, (x, y), [self.visible_sprites, self.attackable_sprites], self.obstacle_sprites, self.damage_player, self.trigger_death_particles)
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
@@ -92,7 +96,13 @@ class Level:
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
             # spawn particles
+            pos = self.player.rect.center
+            groups = [self.visible_sprites]
+            self.animation_player.create_particles(attack_type, pos, groups)
 
+    def trigger_death_particles(self, pos, particle):
+        self.animation_player.create_particles(particle, pos, self.visible_sprites)
+        
     def player_attack_logic(self):
         if self.attack_sprites:
             for attack_sprite in self.attack_sprites:
@@ -100,6 +110,11 @@ class Level:
                 if collision_sprites:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
+                            pos = target_sprite.rect.center
+                            offset = pygame.math.Vector2(0, 75)
+                            group = [self.visible_sprites]
+                            for leaf in range(randint(3, 6)):
+                                self.animation_player.create_grass_particles(pos - offset, group)
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player, attack_sprite.sprite_type)
